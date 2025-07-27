299bb52e3f24db50a39c0ee1645a6fa9d05a3087
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-07-27 15:13:00 -0400

    Adjust settings

--- src/settings.py
+++ src/settings.py
@@ -5,8 +5,17 @@ config.read('config.ini')
 
 window_check_title = config.get("Settings", "window_title")
 ignore_list = config.get("Settings", "ignore_list").split(',')
+
 contour_area_threshold = int(config.get("Settings", "contour_area_threshold"))
+
 debug_windows = bool(config.get("Settings", "debug_windows"))
 play_alarm = bool(config.get("Settings", "play_alarm"))
 click_interval = float(config.get("Settings", "click_interval"))
-alarm_threshold = int(config.get("Settings", "alarm_threshold"))
+
+alarm_time = int(config.get("Settings", "alarm_time"))
+eat_time = int(config.get("Settings", "eattime"))
+
+pause_keybind = config.get("Settings", "pause_keybind")
+stop_keybind = config.get("Settings", "stop_keybind")
+rod_equip_keybind = config.get("Settings", "rod_equip_keybind")
+eat_equip_keybind = config.get("Settings", "eat_equip_keybind")
